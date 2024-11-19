38f782c2ba55faa7e2deb64b51df872153187000
Jhoan Serrano <jms.co89@gmail.com>
2024-11-19 16:30:57 +0000

    Game Mode

---
+++ Source/DesertRacer/DesertRacerGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DesertRacerGameMode.generated.h"
+
+UCLASS()
+class DESERTRACER_API ADesertRacerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
